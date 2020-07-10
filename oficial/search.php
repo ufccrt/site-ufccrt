50476e09214e87cb7069290fcfafa7387a0a9ac5
Leonardo Dutra <leonardo.dutra@crateus.ufc.br>
2020-07-10 18:34:07 -0300

    #21 - melhorar visibilidade do código para encontrar possíveis problemas

--- oficial/search.php
+++ oficial/search.php
@@ -1,85 +1,157 @@
 <?php get_header(); ?>
 
+
+
+
+
+
 <div class="container ">
-    <div class="row">
-		<div class="col-md-9" style="min-height: 500px;">
-			<div class="topo-busca">
-			<h4 class="widget-title">Resultado da Busca<span></h4>
-				Para: </br>
-				<span>
-					<?php
-					$termoPesquisado = get_search_query();
-						if ($termoPesquisado!=""){
-							echo $termoPesquisado;
-							 
-						}else {
-							echo "Digite o que você procura no campo de busca, no topo da págiana";
-						}
-					?>
-				</span>
-				
-			</div>
-			
-						
-			<?php
-			
-			while ( have_posts() ) : the_post() ?>
-							<div id="post-<?php the_ID(); ?>" <?php post_class('PesquisaResultadoNovoModelo'); ?> class="col-md-12">
-											<?php if ( $post->post_type == 'post' ) { ?>
-												<span class="entry-date"><?php the_time('d/m/Y'); ?></span>
-											<?php } ?>
-									<div class="titulo_pesquisa col-md-12" >
-											<a href="<?php the_permalink(); ?>" title="<?php printf( __('Permalink to %s', 'your-theme'), the_title_attribute('echo=0') ); ?>" rel="bookmark">
-												<span><?php the_title(); ?> </span> 
-											</a>
-									</div>
-									<div class="col-md-12 tags-resultados-buscas">
-											
-											<span class="tag_postagem">
-													<?php echo get_the_category_list(', '); ?>
-											</span>
-											
-									</div>
-									
-								
-								
-								<div class="entry-summary">
-									<?php //the_excerpt( __( 'Leia mais... <span class="meta-nav">»</span>', 'your-theme' )  ); ?>
-									<?php wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'your-theme' ) . '&after=</div>') ?>
-								</div><!-- .entry-summary -->
-							</div>
-							
-							
-			<?php
-			
-			endwhile;
 
-			?>
-			
 
+
+
+
+
+
+
+
+
+<div class="row">
+
+
+	
+
+<div class="col-md-9" style="min-height: 500px;">
+
+
+
+
+
+
+<div class="topo-busca">
+	<h4 class="widget-title">Resultado da Busca<span></h4>
+		Para: </br>
+		<span>
 			<?php
-					$ver=have_posts();
-					$p=$ver;
-					if($p===true){
-						echo "<p>Fim dos resultados.</p>";
-					}else{
-						echo "<p>Não há resultados para sua busca. Tente outros termos no campo de busca <span class='iconePesquisaForm' style='width: 100px;height: 50;
-						display: inline-block; position: relative;bottom: -27px;'></span>.</p>";
-					};
+			$termoPesquisado = get_search_query();
+				if ($termoPesquisado!=""){
+					echo $termoPesquisado;
+					 
+				}else {
+					echo "Digite o que você procura no campo de busca, no topo da págiana";
+				}
 			?>
-			
-			
-			<?php global $wp_query; $total_pages = $wp_query->max_num_pages; if ( $total_pages > 1 ) { ?>
-				<div id="nav-below" class="navigation">
-				<div class="nav-next"><?php next_posts_link(__( '<span class="meta-nav">Próximos »</span>', 'your-theme' )) ?></div>
-				<div class="nav-previous"><?php previous_posts_link(__( '<span class="meta-nav">« Anterior</span> ', 'your-theme' )) ?></div>
-				
-			<?php } ?>
-		</div>
-		<div class="col-md-3 barra-lateral-direita">
-				<?php include_once 'views/sessoes-home/acessorapido.php'; ?>
-		</div>
-	</div>
+		</span>
+		
 </div>
+<!-- fim da classe topo-busca -->
+	
+				
+	<?php
+		while ( have_posts() ) : the_post() 
+	?>
+
+					<?php //post_class('PesquisaResultadoNovoModelo'); ?>
+
+					<div id="post-<?php the_ID(); ?>" class="col-md-12">
+
+
+									<?php 
+										if ( $post->post_type == 'post' ) { 
+									?>
+
+										<span class="entry-date"><?php the_time('d/m/Y'); ?></span>
+
+									<?php 
+										} 
+									?>
+
+							<div class="titulo_pesquisa col-md-12" >
+
+
+									<a href="<?php the_permalink(); ?>" title="<?php printf( __('Permalink to %s', 'your-theme'), the_title_attribute('echo=0') ); ?>" rel="bookmark">
+										<span><?php the_title(); ?> </span> 
+									</a>
+
+
+							</div>
+							<!-- fim da div titulo_pesquisa col-md-12 -->
+
+
+
+							<div class="col-md-12 tags-resultados-buscas">
+									
+									<span class="tag_postagem">
+											<?php echo get_the_category_list(', '); ?>
+									</span>
+									
+							</div>
+							<!-- final da div col-md-12 tags-resultados-buscas -->
+							
+						
+						
+						<div class="entry-summary">
+							<?php //the_excerpt( __( 'Leia mais... <span class="meta-nav">»</span>', 'your-theme' )  ); ?>
+							<?php wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'your-theme' ) . '&after=</div>') ?>
+						</div><!-- .entry-summary -->
+					</div>
+					
+					
+	<?php
+		endwhile;
+	?>
+	
+
+	<?php
+			// $ver=have_posts();
+			// $p=$ver;
+			if(have_posts()===true){
+				echo "<p>Fim dos resultados.</p>";
+			}else{
+				echo "<p>Não há resultados para sua busca. Tente outros termos no campo de busca <span class='iconePesquisaForm' style='width: 100px;height: 50;
+				display: inline-block; position: relative;bottom: -27px;'></span>.</p>";
+			};
+	?>
+	
+	
+	<?php global $wp_query; $total_pages = $wp_query->max_num_pages; if ( $total_pages > 1 ) { ?>
+
+		<div id="nav-below" class="navigation">
+		<div class="nav-next"><?php next_posts_link(__( '<span class="meta-nav">Próximos »</span>', 'your-theme' )) ?></div>
+		<div class="nav-previous"><?php previous_posts_link(__( '<span class="meta-nav">« Anterior</span> ', 'your-theme' )) ?></div>
+		
+	<?php } ?>
+
+
+
+
+
+
+</div>
+<!-- fim div col-md-9 -->
+
+<div class="col-md-3 barra-lateral-direita">
+		<?php include_once 'views/sessoes-home/acessorapido.php'; ?>
+</div>
+
+
+
+
+</div>
+<!-- fim div row -->
+
+
+
+
+
+
+
+</div>
+<!-- fim div container -->
+
+
+
+
+
 
 <?php get_footer(); ?>
