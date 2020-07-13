7181a75ec044d69074e212f92f6157bafee43312
Leonardo Dutra <leonardo.dutra@crateus.ufc.br>
2020-07-13 14:17:11 -0300

    #28 - corrigido erro de versão do jquery com bootstrap

--- oficial/header.php
+++ oficial/header.php
@@ -22,7 +22,7 @@
     <meta name="keywords" content="Universidade Federal, UFC, Universidade Pública">
     <link rel="icon" href="<?php echo home_url( '/' ); ?>wp-content/themes/ufc-oficial/imgs/indice.png">
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
         integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
