03d1cb17d45f747c649225c3a2220b3ff7119749
Leonardo Dutra <leodutranew@gmail.com>
2021-02-12 13:56:09 -0300

    #45 - melhorias no script para gerar html taes e atualizacao do css para professores e taes

--- scripts/gerar_html_docente.py
+++ scripts/gerar_html_docente.py
@@ -1,15 +1,9 @@
 import json
 
-taes_json = json.load(open('taes.json', 'r', encoding='utf8'))
 docentes_json = json.load(open('professores.json', 'r', encoding='utf8'))
-
-#taes
-taes = json.dumps(taes_json)
-#print(json.dumps(taes_json, indent=4))
 
 #docentes
 docentes = json.dumps(docentes_json)
-#print(json.dumps(docentes_json, indent=4))
 
 docente_html = []
 taes_html = []
@@ -59,9 +53,6 @@ for ijson in docentes_json:
     </div>\n\
     ")
 
-    #.replace('$areas', html_areas)
-
     docente_html.append(modelo_html_docente)
-
 
 arquivo_html_docentes.writelines(docente_html)
