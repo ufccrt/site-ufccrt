0a8338706445a66c72df948bcdfe5a0b854bf7ed
Leonardo Dutra <leodutranew@gmail.com>
2021-03-03 12:44:24 -0300

    #45 - slide atualizado com as noticias e banners

--- oficial/views/sessoes-home/slide-home.php
+++ oficial/views/sessoes-home/slide-home.php
@@ -4,40 +4,84 @@
             <li data-target="#slide-home" data-slide-to="0" class="active"></li>
             <li data-target="#slide-home" data-slide-to="1"></li>
             <li data-target="#slide-home" data-slide-to="2"></li>
+            <li data-target="#slide-home" data-slide-to="3"></li>
+            <li data-target="#slide-home" data-slide-to="4"></li>
+            <li data-target="#slide-home" data-slide-to="5"></li>
+            <li data-target="#slide-home" data-slide-to="6"></li>
         </ol>
        
         <div class=" carousel-inner">
              <!--INICIO de cada item  :   -->
-            <div class="carousel-item active" style="background-image:url(https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg)">
-                <a class="link-slide" href="#">
+             <div class="carousel-item active" style="background-image:url(<?php echo home_url( '/' ); ?>wp-content/themes/ufc-oficial/imgs/slide-home/destaques/startup1.jpg)">
+                <a class="link-slide" href="http://crateus.ufc.br/startup-de-crateus-e-finalista-do-startupce-2020/">
                     <div class="carousel-caption d-none d-md-block container">
-                        <h5>Título do primeiro slide na página inicial </h5>
-                        <p>Alguma descrição curta sobre o assunto do primeiro slide da página inicial</p>
+                        <h5>Startup de Crateús é finalista do StartupCE 2020</h5>
+                        <p>A startup SisBarragens, formada pelos alunos do nosso Campus é uma das 10 finalistas do Programa de Pré-aceleração Startup CE, iniciativa do Serviço Brasileiro de Apoio às Micro e Pequenas Empresas (SEBRAE-CE). (...)</p>
                     </div>
                 </a>
             </div>
             <!--FIM de cada item -->
              <!--INICIO de cada item  :   -->
-            <div class="carousel-item " style="background-image:url(https://wavesound.com.br/wp-content/uploads/2017/07/fundo-slider-01.jpg)">
-                <a href="#">
+             <div class="carousel-item" style="background-image:url(<?php echo home_url( '/' ); ?>wp-content/themes/ufc-oficial/imgs/slide-home/destaques/tecnologia_1.jpg)">
+                <a class="link-slide" href="http://crateus.ufc.br/projeto-da-ufc-em-crateus-desenvolve-solucoes-tecnologicas-para-o-setor-bancario/">
                     <div class="carousel-caption d-none d-md-block container">
-                        <h5>Título do segundo slide na página inicial </h5>
-                        <p>Alguma descrição curta sobre o assunto do segundo slide da página inicial</p>
+                        <h5>Projeto da UFC em Crateús desenvolve soluções tecnológicas para o setor bancário</h5>
+                        <p>Estimular o surgimento de novos bancos digitais e reduzir a exclusão financeira para a população do sertão cearense. Esses são objetivos do projeto Proposal of a Reusable Open Banking Platform to Reduce the Drop-out and Financial Exclusion Rates in Crateús (Proposta de plataforma de banco aberto reutilizável para redução das taxas de evasão e exclusão financeira em Crateús), que teve início no último dia 11, no Campus da UFC em Crateús. (...)</p>
+                    </div>
+                </a>
+            </div>
+            <!--FIM de cada item -->
+             <!--INICIO de cada item  :   -->
+            <div class="carousel-item" style="background-image:url(<?php echo home_url( '/' ); ?>wp-content/themes/ufc-oficial/imgs/slide-home/engenharia_ambiental.jpg)">
+                <a class="link-slide" href="http://crateus.ufc.br/ea">
+                    <div class="carousel-caption d-none d-md-block container">
+                        <h5>Curso de Engenharia Ambiental e Sanitária</h5>
+                        <p>Clique para mais informações sobre o curso</p>
+                    </div>
+                </a>
+            </div>
+            <!--FIM de cada item -->
+             <!--INICIO de cada item  :   -->
+            <div class="carousel-item " style="background-image:url(<?php echo home_url( '/' ); ?>wp-content/themes/ufc-oficial/imgs/slide-home/engenharia_civil.jpg)">
+                <a href="http://crateus.ufc.br/ec">
+                    <div class="carousel-caption d-none d-md-block container">
+                        <h5>Curso de Engenharia Civil </h5>
+                        <p>Clique para mais informações sobre o curso</p>
                     </div>
                 </a>
             </div>
             <!--FIM de cada item -->
             <!--INICIO de cada item  :   -->
-            <div class="carousel-item " style="background-image:url(https://img.olhardigital.com.br/uploads/acervo_imagens/2019/08/r16x9/20190819040121_1200_675_-_inteligencia_artificial.jpg)">
-                <a href="#">
+            <div class="carousel-item " style="background-image:url(<?php echo home_url( '/' ); ?>wp-content/themes/ufc-oficial/imgs/slide-home/ciencia_computacao.jpg)">
+                <a href="http://crateus.ufc.br/cc">
                     <div class="carousel-caption d-none d-md-block container">
-                        <h5>Título do terceiro slide na página inicial </h5>
-                        <p>Alguma descrição curta sobre o assunto do terceiro slide da página inicial</p>
+                        <h5>Curso de Ciência da Computação</h5>
+                        <p>Clique para mais informações sobre o curso</p>
                     </div>
                 </a>
             </div>
             <!--FIM de cada item -->
-            
+            <!--INICIO de cada item  :   -->
+            <div class="carousel-item " style="background-image:url(<?php echo home_url( '/' ); ?>wp-content/themes/ufc-oficial/imgs/slide-home/sis_informacao.jpg)">
+                <a href="http://crateus.ufc.br/si">
+                    <div class="carousel-caption d-none d-md-block container">
+                        <h5>Curso de Sistemas de Informação</h5>
+                        <p>Clique para mais informações sobre o curso</p>
+                    </div>
+                </a>
+            </div>
+            <!--FIM de cada item -->
+            <!--INICIO de cada item  :   -->
+            <div class="carousel-item " style="background-image:url(<?php echo home_url( '/' ); ?>wp-content/themes/ufc-oficial/imgs/slide-home/minas.jpg)">
+                <a href="http://crateus.ufc.br/em">
+                    <div class="carousel-caption d-none d-md-block container">
+                        <h5>Curso de Engenharia de Minas</h5>
+                        <p>Clique para mais informações sobre o curso</p>
+                    </div>
+                </a>
+            </div>
+            <!--FIM de cada item -->
+
         </div>
         
     </div>
